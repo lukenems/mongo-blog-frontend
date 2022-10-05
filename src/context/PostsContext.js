e8848a70470d0b71586dcaf2a7858f4447dc4041
Luke Nemy <lukenemy7@gmail.com>
2022-10-05 10:39:57 -1000

    working on integrating context; fixed mango icon and site metadata

---
+++ src/context/PostsContext.js
@@ -0,0 +1,34 @@
+import { createContext, useReducer } from "react";
+
+export const PostContext = createContext();
+export const postsReducer = (state, action) => {
+  switch(action.type) {
+    case 'GET_POSTS':
+      return {
+        posts: action.payload
+      }
+    case 'CREATE_POST':
+      return {
+        posts: [action.payload, ...state.posts]
+      }
+    default:
+       return state
+  }
+}
+
+
+export const PostContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(postsReducer, {
+    posts: null
+  })
+
+  dispatch({type:'', payload:[]})
+
+  return (
+    <PostContext.Provider value={{state, dispatch}}>
+      { children }
+    </PostContext.Provider>
+  )
+
+}
+
