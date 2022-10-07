e16e31f3bda797801080d2498d92153f0b50953a
Luke Nemy <lukenemy7@gmail.com>
2022-10-06 14:57:58 -1000

    falling mangos is a goooo

--- src/components/css/fallingMongoBkgd.js
+++ src/components/css/fallingMongoBkgd.js
@@ -4,138 +4,119 @@ import { mongoPositions } from '../../assets/data/mongoPositions';
 
 const InitMongos = () => {
   const mongoPos = mongoPositions;
+  const divs = document.querySelectorAll('.mongo');
 
-  const updatePos = (div) => {
-    let mongoIndex = Math.round(Math.random()*10);
-
-    let locX = mongoPos[mongoIndex].x
-    let locY = mongoPos[mongoIndex].y
-
-    div.style.left = locX;
-    div.style.top = locY;
+  const updatePos = () => {
+    console.log('called')
+    for (let div of divs) {
+      let mongoIndex = Math.round(Math.random()*10);
+  
+      let locX = mongoPos[mongoIndex].x
+      let locY = mongoPos[mongoIndex].y
+  
+      div.style.left = locX;
+      div.style.top = locY;
     }
+  }
 
   return (
     <div>
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongo'
-        onAnimationEnd={(e) => {updatePos(e.target)}}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongo'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongo'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongo'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongoSecond'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongoSecond'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongoSecond'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongoSecond'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongoThird'
-        onAnimationEnd={updatePos}
+        onAnimationIteration={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongoThird'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongoThird'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongoThird'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongo'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongo'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongo'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongo'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongoSecond'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongoSecond'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongoSecond'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongoSecond'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongoThird'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongoThird'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongoThird'
-        onAnimationEnd={updatePos}
         />
       <img src={mongo}
         alt='falling mongo' 
         className='mongo falling-mongoThird'
-        onAnimationEnd={updatePos}
         />
     </div>
   )
