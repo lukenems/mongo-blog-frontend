319fae459f267fcd89fe9071f7a47daef63c8eb0
Luke Nemy <lukenemy7@gmail.com>
2022-10-07 18:37:38 -1000

    added useLogin hook

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,41 +1,20 @@
 import { useState } from "react";
-import { useAuthContext } from "../hooks/useAuthContext";
+import { useLogin } from "../hooks/useLogin";
+
 import './css/loginForm.css'
 
 const LoginForm = () => {
-  const { dispatch } = useAuthContext();
   let [author, setAuthor] = useState('');
   const [pwd, setPwd] = useState('');
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(null);
+  const { login, isLoading, error } = useLogin();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setIsLoading(true)
     author === '' 
       ? author = 'dev'
       : author = ''
 
-    const response = await fetch('/api/auth/login', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({author, password:pwd})
-    })
-    const json = await response.json();
-
-    if(!response.ok) {
-      setError(json.error);
-      setAuthor('');
-      setPwd('');
-      setIsLoading(false);
-    } else {
-      localStorage.setItem('user', JSON.stringify(json));
-      setAuthor('');
-      setPwd('');
-      setError(null);
-      setIsLoading(false);
-      dispatch({type: 'LOGIN', payload:json});
-    }
+    await login(author, pwd)    
   }
 
   return (
