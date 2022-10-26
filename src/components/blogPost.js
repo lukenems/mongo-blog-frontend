2a611c4ab0a829d869e9a603b99a1a0f8dc64315
Luke Nemy <lukenemy7@gmail.com>
2022-10-26 10:34:06 -1000

    modile background update; added code for linebreaks in posts

--- src/components/blogPost.js
+++ src/components/blogPost.js
@@ -2,7 +2,7 @@ const BlogPost = ({ post }) => {
   return (
     <div className="post-details">
       <h3 className="title">{post.title}</h3>
-      <p className="content">{post.content}</p>
+      <p className="content" dangerouslySetInnerHTML={{ __html: post.content }}></p>
       <h3 className="date">{post.dreamspellDate}</h3>
     </div>
   )
